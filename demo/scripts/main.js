8e756f8e48416d9b80a8b6e0d4e07552b6cf61fc
stat92 <makha.a-92@mail.ru>
2017-05-27 16:03:43 +0300

    first upload

---
+++ demo/scripts/main.js
@@ -0,0 +1,118 @@
+document.addEventListener('DOMContentLoaded',function() {
+
+	composersTemplate = '<div data-composerid="{composerId}" class="composers-item">'+
+	                       '<img class="composers-img" src="{source}">'+
+	                       '<p class="composers-data">'+
+	                       		'<span>{name}</span><br><span>{surname}</span><br>'+
+	                       		'<span>{birthday}-{deathday}</span>'
+	                       '</p>'+
+	                    '</div>';
+
+	composers = new MultiLinkSlider({
+		container: document.getElementById('composers'), 
+		template: composersTemplate, 
+		minWidth: 200
+	});
+
+	composers.render([{
+					   	name: 'Joseph',
+					   	surname: 'Haydn',
+					   	source: 'styles/img/Haydn.jpg',
+					   	composerId: 1,
+					   	birthday: 1732,
+					   	deathday: 1809,
+						listeners: {
+							click: function (item) {
+								document.getElementById('composer').innerHTML = item.name + ' ' + item.surname
+							}
+						}
+					},
+					{ 
+					   	name: 'Qurmangazy',
+					   	surname: 'Sagyrbaiuly',
+					   	source: 'styles/img/Sagyrbaiuly.png',
+					   	composerId: 2,
+					   	birthday: 1818,
+					   	deathday: 1896,
+						listeners: {
+							click: function (item) {
+								document.getElementById('composer').innerHTML = item.name + ' ' + item.surname
+							}
+						}
+					},
+					{ 
+					   	name: 'Pyotr',
+					   	surname: 'Tchaikovsky',
+					   	source: 'styles/img/Tschaikowski.jpg',
+					   	composerId: 3,
+					   	birthday: 1840,
+					   	deathday: 1893,
+						listeners: {
+							click: function (item) {
+								document.getElementById('composer').innerHTML = item.name + ' ' + item.surname
+							}
+						}
+					},
+					{ 
+					   	name: 'Nikolai',
+					   	surname: 'Rimsky-Korsakov',
+					   	source: 'styles/img/Rimsky.png',
+					   	composerId: 4,
+					   	birthday: 1844,
+					   	deathday: 1908,
+						listeners: {
+							click: function (item) {
+								document.getElementById('composer').innerHTML = item.name + ' ' + item.surname
+							}
+						}
+					}]);
+
+
+
+	compositionsTemplate = '<div class="compositions-data">'+
+	                       		'<span data-compositionid="{compositionId}">{title}</span>'+
+	                    	'</div>';
+
+	compositions = new MultiLinkSlider({
+		container: document.getElementById('compositions'), 
+		template: compositionsTemplate, 
+		minWidth: 120
+	});
+
+	compositions.render([{ 
+					   	compositionId: 1,
+					   	title: 'Composition 1',
+						listeners: {
+							click: function (item) {
+								document.getElementById('composition').innerHTML = item.title
+							}
+						}
+					}, 
+					{
+						compositionId: 1,
+					   	title: 'Composition 2',
+						listeners: {
+							click: function (item) {
+								document.getElementById('composition').innerHTML = item.title
+							}
+						}
+					},
+					{
+						compositionId: 1,
+					   	title: 'Composition 3',
+						listeners: {
+							click: function (item) {
+								document.getElementById('composition').innerHTML = item.title
+							}
+						}
+					},
+					{
+						compositionId: 1,
+					   	title: 'Composition 4',
+						listeners: {
+							click: function (item) {
+								document.getElementById('composition').innerHTML = item.title
+							}
+						}
+					}]);
+})
---
+++ source/mls.js
@@ -0,0 +1,205 @@
+var MultiLinkSlider = (function () {
+
+	function MultiLinkSlider (config) {
+		
+		if (!config.container || !config.template) {
+			throw Error('Container and template is needed');
+		}
+
+		this.outerContainer = config.container;
+		this.itemsTemplate = config.template;
+		this.whereIs = 0;
+		this.length = 0;
+		this.numb = MultiLinkSlider.countSlider++;	
+		this.minWidth = config.minWidth || 250;
+		this.amountToShow = config.amountToShow || undefined
+	}
+
+	MultiLinkSlider.countSlider = 1;
+
+	MultiLinkSlider.prototype = {
+		
+		constructor: MultiLinkSlider,
+
+		getCount: function () {
+			return this.length
+		},
+
+		getTemplate: function(items) {
+			var me = this,
+				div, ul;
+
+			div = document.createElement('div');
+			div.className += ' mls-div mls-div-' + me.numb;
+
+			ul = document.createElement('ul');
+			ul.className += ' mls-ul mls-ul-' + me.numb;
+
+			div.appendChild(ul);
+
+			items.forEach(function (item) {
+				ul.appendChild(me.getLiTemplate(item))
+			})
+
+			return div;
+		},
+
+		getLiTemplate: function (item) {
+			var li = document.createElement('li'),
+				listeners = item['listeners']
+
+			li.className += ' mls-li mls-li-' + this.numb;
+
+			li.innerHTML = this.generateItem(item)
+
+			if (listeners) 
+			{
+				for (key in listeners)
+				{
+					if (listeners.hasOwnProperty(key)) 
+					{
+						li.addEventListener(key, listeners[key].bind(li, item))
+					}
+				}
+			}
+
+			return li;
+		},
+
+		generateItem: function (item) {
+			var temporary = this.itemsTemplate, key, value;
+
+			for (key in item)
+			{
+			    if (item.hasOwnProperty(key)) 
+				{
+					value = item[key];
+
+					if (value === undefined || value === null) value = "";
+
+			    	temporary = temporary.replace(new RegExp('{'+key+'}'), value)
+			    }
+			}
+
+			return temporary;
+		},
+
+		getAmountOfItemsToShow: function () {
+			var me = this,
+			    itemsToShow,
+				all;
+
+			if (me.amountToShow) {
+				itemsToShow = me.amountToShow
+			} else {
+				itemsToShow = Math.floor(me.outerContainer.offsetWidth/me.minWidth);
+
+				if (itemsToShow == 0) 
+					itemsToShow++
+				else if (itemsToShow > (all = me.getCount())) 
+					itemsToShow = all;
+			}
+			
+			return itemsToShow;
+		},
+
+		getUlWidth: function () {
+			return this.getLiWidth()*this.getCount();
+		},
+
+		getLiWidth: function () {		
+			return Math.floor(this.outerContainer.offsetWidth/this.getAmountOfItemsToShow());
+		},
+
+		getLiPercentWidth: function () {
+			return 100/this.getCount();
+		},
+
+		render: function (items) {
+			var me = this,
+				lis;
+				
+			me.length = items.length;
+			me.outerContainer.appendChild(me.getTemplate(items));
+			
+			me.leftButton = me.createButton('left');
+			me.rightButton = me.createButton('right');
+
+			me.leftButton.addEventListener('click', function () { me.move(-1); })
+			me.rightButton.addEventListener('click', function () { me.move(1); })
+			
+			lis = document.querySelectorAll('.mls-li-'+me.numb);
+
+			document.querySelector('.mls-ul-'+me.numb).style.width = me.getUlWidth()+'px'
+			
+			lis.forEach(function(li) {
+				li.style.width = me.getLiPercentWidth()+'%'
+			})
+
+			me.checkButtons();
+
+			window.addEventListener('resize', function()
+			{
+				var style = document.querySelector('.mls-ul-'+me.numb).style;
+
+				style.width = me.getUlWidth()+'px'
+				style.left = (me.whereIs*me.getLiWidth())+'px'
+
+				me.checkButtons();
+			});
+		},
+
+		createButton: function (direction) {
+			var div = document.createElement('div'),
+				parent = document.querySelector('.mls-div-'+this.numb),
+				ul = document.querySelector('.mls-ul-'+this.numb);
+
+			div.className += 'mls-button-'+direction;
+
+			parent.insertBefore(div, ul);
+
+			return div;
+		},
+
+		append: function (items) {
+			var me = this,
+				temp = document.createElement('div'),
+				ul = document.querySelector('.mls-ul-'+me.numb);
+
+			me.length += items.length;
+
+			items.forEach(function (item) {
+				ul.appendChild(me.getLiTemplate(item))
+			})
+
+			ul.style.width = me.getUlWidth()+'px'
+			
+			document.querySelectorAll('.mls-li-'+me.numb).forEach(function(li) {
+				li.style.width = me.getLiPercentWidth()+'%'
+			})
+
+			me.checkButtons();
+		},
+
+		move: function (direction) {
+			var me = this;
+
+			me.whereIs -= direction;
+			me.checkButtons();
+			
+			document.querySelector('.mls-ul-'+me.numb).style.left = (me.whereIs*me.getLiWidth())+'px'
+		},
+
+		checkButtons: function () {
+			var me = this,
+				hasRight = (me.whereIs+me.getCount()-me.getAmountOfItemsToShow()) > 0,
+				hasLeft  = (me.whereIs < 0);
+
+			me.rightButton.style.display = hasRight ? 'block' : 'none'
+			me.leftButton.style.display =  hasLeft  ? 'block' : 'none'
+		}
+	}
+
+	return MultiLinkSlider;
+}())
+
